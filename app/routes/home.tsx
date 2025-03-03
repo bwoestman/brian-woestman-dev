18146094548e223b51ebcd5e5e8b5452c3254913
Brian Woestman <woestmab@gmail.com>
2025-03-03 17:28:17 -0600

    Base mobile with some content

--- app/routes/home.tsx
+++ app/routes/home.tsx
@@ -1,13 +1,44 @@
+import * as React from "react";
+import Timeline from "@mui/lab/Timeline";
 import type { Route } from "./+types/home";
-import { Welcome } from "../welcome/welcome";
+import {
+  TimelineConnector,
+  TimelineContent,
+  TimelineDot,
+  TimelineItem,
+  TimelineSeparator,
+} from "@mui/lab";
+import Experience from "./experience";
 
 export function meta({}: Route.MetaArgs) {
   return [
-    { title: "New React Router App" },
+    { title: "Brian Woestman - Dev" },
     { name: "description", content: "Welcome to React Router!" },
   ];
 }
 
 export default function Home() {
-  return <Welcome />;
+  return (
+    <main className="grid">
+      <section className="p-3">
+        <h1 className="text-4xl font-bold">Brian Woestman</h1>
+        <h2 className="text-2xl">Frontend Developer </h2>
+        <article className="p-2">
+          <p>
+            Stinky cat leave fur on owners clothes yet refuse to come home when
+            humans are going to bed; stay out all night then yowl like i am
+            dying at 4am please stop looking at your phone and pet me. Kitty
+            power in the middle of the night i crawl onto your chest and purr
+            gently to help you sleep. Human is behind a closed door, emergency!
+            abandoned! meeooowwww!!!
+          </p>
+          <p className="pt-2">Feel free to reach out!</p>
+        </article>
+      </section>
+      <img src="/assets/wall-burst.png" alt="" />
+      <section className="p-3">
+        <Experience />
+      </section>
+    </main>
+  );
 }
