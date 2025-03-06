8cffd254616ff3c18dc465182ca3602fa858e61f
Brian Woestman <woestmab@gmail.com>
2025-03-06 17:46:43 -0600

    Add confetti from differnt angles

--- app/routes/home.tsx
+++ app/routes/home.tsx
@@ -6,6 +6,7 @@ import { useEffect } from "react";
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
 import { faGithub, faLinkedin } from "@fortawesome/free-brands-svg-icons";
 
+
 export function meta({}: Route.MetaArgs) {
   return [
     { title: "Brian Woestman - Dev" },
@@ -13,18 +14,45 @@ export function meta({}: Route.MetaArgs) {
   ];
 }
 
+const colors = [
+  "#2323ff",
+  "#eb00be",
+  "#ff0079",
+  "#ff6f47",
+  "#ffbd3a",
+  "#f9f871",
+];
+
 export default function Home() {
   useEffect(() => {
-    confetti({ origin: { y: 0.7 } });
+    confetti({
+      angle: -45,
+      spread: 120,
+      origin: { x: 0, y: 0 },
+      colors,
+      scalar: 1.2,
+    });
+    confetti({
+      angle: -90,
+      spread: 120,
+      origin: { x: 0.5, y: 0 },
+      colors,
+    });
+    confetti({
+      angle: -135,
+      spread: 120,
+      origin: { x: 1, y: 0 },
+      colors,
+    });
   }, []);
 
   return (
     <main className="m-auto grid max-w-screen-lg">
       <section className="m-5">
-        <div className="flex justify-around md:justify-between">
+        <div className="flex justify-between">
           <div>
             <h1 className="text-4xl font-bold sm:text-5xl">Brian Woestman</h1>
-            <h2 className="text-2xl">Senior Software Engineer</h2>
+            <h2 className="text-lg sm:text-2xl">Senior Software Engineer</h2>
           </div>
           <div className="flex flex-col justify-around">
             <a href="https://www.linkedin.com/in/bdub1897/">
@@ -36,24 +64,27 @@ export default function Home() {
           </div>
         </div>
 
-        <article className="mt-3">
-          <p>
-            Full stack engineer with a decade of experience continuously
-            delivering performant, scalable and maintainable software. At A2Z
-            Sync I just wrapped up the launch of Amazon Autos, amplifying the
-            car buying experience to a new level!
-          </p>
-        </article>
-        <div>
-          <p className="mt-3 text-lg">Let's build something together!</p>
+        <article className="mt-3 sm:grid sm:grid-cols-2">
+          <div className="mt-5">
+            <p>
+              Full stack engineer with a decade of experience continuously
+              delivering performant, scalable and maintainable software. At A2Z
+              Sync I just wrapped up the launch of Amazon Autos, amplifying the
+              car buying experience to a new level!
+            </p>
+            <p className="mt-8 text-center text-lg sm:text-left">
+              Let's build something together!
+            </p>
+          </div>
+
           <img
-            className="mx-auto mt-3 h-auto"
+            className="mt-3"
             src="/assets/wall-burst.png"
             alt="Brian bursting through the wall!"
           />
-        </div>
+        </article>
         {employmentData.map((job) => (
-          <ExperienceCard {...{ ...job }} />
+          <ExperienceCard key={job.company} {...{ ...job }} />
         ))}
       </section>
     </main>
