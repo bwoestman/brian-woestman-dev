5ff86d4045fae7c3df77f8636c7061f9fb13e115
Brian Woestman <woestmab@gmail.com>
2025-03-06 12:59:11 -0600

    add confetti

--- app/routes/home.tsx
+++ app/routes/home.tsx
@@ -1,6 +1,8 @@
 import type { Route } from "./+types/home";
+import confetti from "canvas-confetti";
 import ExperienceCard from "~/components/ExperienceCard";
 import employmentData from "../data/employment";
+import { useEffect } from "react";
 
 export function meta({}: Route.MetaArgs) {
   return [
@@ -10,12 +12,17 @@ export function meta({}: Route.MetaArgs) {
 }
 
 export default function Home() {
+  useEffect(() => {
+    confetti({ origin: { y: 0.7 } });
+  }, []);
+
   return (
     <main className="grid">
       <section className="3 m-5">
         <h1 className="text-4xl font-bold sm:text-5xl">Brian Woestman</h1>
         <h2 className="text-2xl">Senior Software Engineer</h2>
         <h3 className="text-lg">Tech Lead</h3>
+
         <article className="mt-3">
           <p>
             Full stack engineer with a decade of experience continuously
@@ -23,13 +30,16 @@ export default function Home() {
             Sync I just wrapped up the launch of Amazon Autos, amplifying the
             car buying experience to a new level!
           </p>
+        </article>
+
+        <div>
           <p className="mt-3 text-lg">Let's build something together!</p>
           <img
-            className="mt-3"
+            className="mx-auto mt-3 h-auto"
             src="/assets/wall-burst.png"
             alt="Brian bursting through the wall!"
           />
-        </article>
+        </div>
 
         {employmentData.map((job) => (
           <ExperienceCard {...{ ...job }} />
